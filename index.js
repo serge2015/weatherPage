168a11e3f4d7f24eb926f0014907254171e6a31c
Sergio Dussan <sdussan@duck.com>
2024-11-26 17:09:44 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+let tempUnits;
+let units;
+let unitsAlt;
+let favorite;
+
+function setFav() {
+  sessionStorage.setItem("favorite", document.getElementById("city").innerText);
+}
+
+function loadFav(){
+    favorite = sessionStorage.getItem("favorite");
+    if (favorite)
+        getWeather(favorite);
+}
+
+async function getWeather(location) {
+    document.getElementById('icon').style.display = "none";
+    document.querySelector('.temp').style.display = "none";
+    document.getElementById('city').style.display = "none";
+    document.querySelector('.fav_city').style.display = "none";
+    document.querySelector('.five_day_forecast').style.display = "none";
+    document.querySelector('.one').style.display = "block";
+    document.querySelector('.two').style.display = "block";
+    const weather = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=79c219b599e24530ab245407242611&q=${location}&days=5&aqi=no&alerts=no`)
+    const weatherData = await weather.json();
+    document.getElementById('F').checked ? (
+        tempUnits = `${weatherData.current.temp_f}`,
+        units = "F",
+        unitsAlt = "C"
+    ): 
+    (
+        tempUnits = `${weatherData.current.temp_c}`,
+        units = "C",
+        unitsAlt = "F"
+    );
+    document.querySelector('.temp').innerHTML = `${tempUnits}&#176; ${units}<button class="changeUnits" onclick="changeUnits()">${unitsAlt}</button>`;
+    document.getElementById('icon').src = `${weatherData.current.condition.icon}`;
+    document.getElementById('icon').style.opacity = 1;
+    document.getElementById('city').innerHTML = `${location}`;
+    if (favorite === location) {
+        document.getElementById('city').innerHTML += "&nbsp;&#11088;"
+    }
+    weatherData.forecast.forecastday.forEach((element, index) => {
+        units ==='F' ? (
+            document.getElementById("day"+index).innerHTML = `${weatherData.forecast.forecastday[index].day.maxtemp_f}<br />${weatherData.forecast.forecastday[index].day.mintemp_f}`
+        ):
+        (document.getElementById("day"+index).innerHTML = `${weatherData.forecast.forecastday[index].day.maxtemp_c}<br />${weatherData.forecast.forecastday[index].day.mintemp_c}`
+        );
+        document.getElementById("icon"+index).src = `${weatherData.forecast.forecastday[index].day.condition.icon}`;
+        document.getElementById("icon"+index).style.opacity = 1;
+    });
+    document.querySelectorAll("li:not(:last-child)")
+  .forEach((element) => {
+    element.style.borderRight = "1px solid olivedrab", 
+    element.style.animation = "fadeIn 1s ease-in"
+    });
+    document.getElementById('fav_city').disabled = false;
+    document.getElementById('fav_city').checked = false;
+    document.getElementById('icon').style.display = "block";
+    document.getElementById('icon').style.animation = "fadeIn 1s ease-in";
+    document.querySelector('.temp').style.display = "block";
+    document.querySelector('.temp').style.animation = "fadeIn 1s ease-in";
+    document.getElementById('city').style.display = "block";
+    document.getElementById('city').style.animation = "fadeIn 1s ease-in";
+    document.querySelector('.fav_city').style.display = "block";
+    document.querySelector('.fav_city').style.animation = "fadeIn 1s ease-in";
+    document.querySelector('.five_day_forecast').style.display = "flex";
+    document.querySelector('.five_day_forecast').style.animation = "fadeIn 1s ease-in";
+    document.querySelector('.one').style.display = "none";
+    document.querySelector('.two').style.display = "none";
+
+}   
+
+async function changeUnits(){
+    const weather = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=79c219b599e24530ab245407242611&q=${location}&days=5&aqi=no&alerts=no`)
+    const weatherData = await weather.json();
+    units === "C" ? (
+        tempUnits = `${weatherData.current.temp_f}`,
+        units = "F",
+        unitsAlt = "C"
+    ): 
+    (
+        tempUnits = `${weatherData.current.temp_c}`,
+        units = "C",
+        unitsAlt = "F"
+    );
+    document.querySelector('.temp').innerHTML = `${tempUnits}&#176; ${units}<button class="changeUnits" onclick="changeUnits()">${unitsAlt}</button>`;
+    weatherData.forecast.forecastday.forEach((element, index) => {
+        units ==='F' ? (
+            document.getElementById("day"+index).innerHTML = `${weatherData.forecast.forecastday[index].day.maxtemp_f}<br />${weatherData.forecast.forecastday[index].day.mintemp_f}`
+        ):
+        (document.getElementById("day"+index).innerHTML = `${weatherData.forecast.forecastday[index].day.maxtemp_c}<br />${weatherData.forecast.forecastday[index].day.mintemp_c}`
+        );
+    });
+}
